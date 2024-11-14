c868bc409b3021799f7e55e57a2f2f9dc3db906b
Ashless Scythe <drakesav@gmail.com>
2024-11-14 17:56:38 +0000

    themesprovider type err

--- src/components/providers/theme-provider.tsx
+++ src/components/providers/theme-provider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({
   ...props
 }: {
   children: React.ReactNode;
-  attribute?: string;
+  attribute: "class" | "data-theme";
   defaultTheme?: string;
   enableSystem?: boolean;
   disableTransitionOnChange?: boolean;
